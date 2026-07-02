7022afd220b0bc3c34c60b920f95f6d4e774db99
Patryk Walaszkowski <pwalaszkowski@gmail.com>
2026-07-02 18:34:21 +0200

    pytest-capabilities

--- test_calc.py
+++ test_calc.py
@@ -46,18 +46,29 @@ def logger():
 # Testowania API przez requests
 
 # Kod testów
+@pytest.mark.codebrainers
 def test_add(logger, prepare_data):
     logger.info("Test Started")
     logger.info(f"Data used for testing {prepare_data[0], prepare_data[1]}")
     assert add(prepare_data[0], prepare_data[1]) == 10, "Komunikat błędu"
+    logger.debug("Debug Message")
+    logger.error("Error Message")
+    logger.warning("Warning Message")
     logger.info("Test Finished")
 
+@pytest.mark.TEST_12345
 def test_substract(prepare_data):
     assert subtract(prepare_data[0], prepare_data[1]) == 0 
 
-
-def test_multiply(prepare_data):
-    assert multiply(prepare_data[0], prepare_data[1]) == 25
+@pytest.mark.parametrize("a, b, result",[
+                        (2, 2, 4),
+                        (1, 0, 1),
+                        (3, 3, 9),
+                        (5, 5, 25),
+                        (10, 10, 100),
+])
+def test_multiply(a, b, result):
+    assert multiply(a,b) == result
 
 
 def test_divide(prepare_data):
